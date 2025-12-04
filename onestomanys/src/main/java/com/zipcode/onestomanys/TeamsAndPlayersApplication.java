46af58914014902d4ff9cbcc6a4eb70faeb28573
deveshmur <dmdevesh@gmail.com>
2025-12-04 15:30:05 -0500

    set up spring project

---
+++ onestomanys/src/main/java/com/zipcode/onestomanys/TeamsAndPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.onestomanys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsAndPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsAndPlayersApplication.class, args);
+	}
+
+}
