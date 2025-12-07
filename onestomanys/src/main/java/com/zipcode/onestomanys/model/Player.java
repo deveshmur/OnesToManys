af89546277c352807d7492cb6671927af0570e33
deveshmur <dmdevesh@gmail.com>
2025-12-07 16:53:43 -0500

    added player.java class

--- onestomanys/src/main/java/com/zipcode/onestomanys/model/Player.java
+++ onestomanys/src/main/java/com/zipcode/onestomanys/model/Player.java
@@ -1,5 +1,89 @@
 package com.zipcode.onestomanys.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "players")
 public class Player {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_id")
+    private Long playerId;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String position;
+
+    @Column(name = "jersey_number", nullable = false)
+    private Integer jerseyNumber;
+
+    @Column(name = "height_inches")
+    private Integer heightInches;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    @JsonIgnore          
+    private Team team;
+
+    public Player() {
+    }
+
+    public Player(String name, String position, Integer jerseyNumber, Integer heightInches) {
+        this.name = name;
+        this.position = position;
+        this.jerseyNumber = jerseyNumber;
+        this.heightInches = heightInches;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Long playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public void setJerseyNumber(Integer jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
+
+    public Integer getHeightInches() {
+        return heightInches;
+    }
+
+    public void setHeightInches(Integer heightInches) {
+        this.heightInches = heightInches;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
