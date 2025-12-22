523539edba84daab8127d86449055123891868df
deveshmur <dmdevesh@gmail.com>
2025-12-21 21:49:50 -0500

    fixed vite, chakra, react compatibility issues

---
+++ onestomanys/src/react/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", padding: "20px" }}>
+        React + Chakra is working!
+      </h1>
+    </div>
+  );
+}
