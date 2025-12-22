300b6c2198701f9099f062abc88b193ac3fb366b
deveshmur <dmdevesh@gmail.com>
2025-12-22 01:17:25 -0500

    updated home page text

--- onestomanys/src/react/src/pages/Home.jsx
+++ onestomanys/src/react/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Box p={10} textAlign="center">
       <Heading mb={4} color="black">
-        Welcome to the NFL Browser
+        Welcome to the 2024-2025 NFL Database
       </Heading>
       <Text fontSize="lg" color="gray.900">
         Explore NFL teams and their players. Click “Database” to begin.
